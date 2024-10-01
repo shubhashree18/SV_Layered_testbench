0ff550b32caa79bdcc858292586722d4f90f19cc
shubhashree18 <shubhashreejs18@gmail.com>
2024-10-01 16:30:52 +0530

    Update transaction.sv

--- Layered_testbench/transaction.sv
+++ Layered_testbench/transaction.sv
@@ -1,1 +1,19 @@
-
+class transaction; //packet
+  
+// Stimulus are declared with rand keyword
+  
+rand bit reset;
+rand bit clk;
+rand bit d;
+  
+bit q;
+  
+//Function for Displaying values 
+  function void display (string name);
+    $display("----------------------------")
+    $display("%s", name);
+    $display("----------------------------")
+    $display("clk = %0d, reset = %0d, clk, reset, d); 
+             $display("q = %0d",q);
+  endfunction
+endclass
