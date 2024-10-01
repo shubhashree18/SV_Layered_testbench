f68d332a62148a48da4b588a98e68136374a6e97
shubhashree18 <shubhashreejs18@gmail.com>
2024-10-01 17:24:24 +0530

    Create generator.sv

---
+++ Layered_testbench/Half_adder/generator.sv
@@ -0,0 +1,18 @@
+class generator;
+  transaction trans; //Handle of Transaction class
+  mailbox gen2driv; //Mailbox declaration
+  function new (mailbox gen2driv); //creation of mailbox and constructor
+    this. gen2driv = gen2driv;
+  endfunction
+  
+  task main();
+    repeat (1)
+      begin
+        trans = new();
+        trans.randomize(); 
+        trans.display("Generator");
+        gen2driv.put(trans);
+      end
+    endtask
+
+endclass
